969bd611fb724eab22b92ab4b934997257ee6edc
David-Sang96 <luainawl@gmail.com>
2024-01-25 10:28:32 +0800

    global-object

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("hello world");
+// console.log(global);
+//node runs in computer
+
+// global.setTimeout(() => {
+//   console.log("hello");
+// }, 2000);
+
+// const interval = global.setInterval(() => {
+//   console.log("hi");
+// }, 1000);
+
+// global.setTimeout(() => {
+//   clearInterval(interval);
+// }, 5000);
+
+console.log(__dirname); //absolute path
+console.log(__filename); //absolute path and file name
