0df192729ac4c82437387503f6171024aefec435
David-Sang96 <luainawl@gmail.com>
2024-01-25 10:36:37 +0800

    node-module-system

---
+++ module1.js
@@ -0,0 +1,5 @@
+const myName = "david";
+const myAge = 27;
+const myHobby = "coding";
+
+module.exports = { myName, myAge, myHobby };
---
+++ module2.js
@@ -0,0 +1,5 @@
+// const obj = require("./module1");
+const { myName, myAge, myHobby } = require("./module1");
+
+// console.log(obj.myName, obj.myAge);
+console.log(myName, myAge, myHobby);
