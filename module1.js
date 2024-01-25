23f23283f21c674465ccf9d071b16071360e5672
David-Sang96 <luainawl@gmail.com>
2024-01-25 11:06:06 +0800

    file-system

--- module1.js
+++
@@ -1,5 +0,0 @@
-const myName = "david";
-const myAge = 27;
-const myHobby = "coding";
-
-module.exports = { myName, myAge, myHobby };
--- module2.js
+++
@@ -1,5 +0,0 @@
-// const obj = require("./module1");
-const { myName, myAge, myHobby } = require("./module1");
-
-// console.log(obj.myName, obj.myAge);
-console.log(myName, myAge, myHobby);
