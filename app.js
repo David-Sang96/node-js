30deb22f88ee3ac30875d60ad4da741ef1f517c7
David-Sang96 <luainawl@gmail.com>
2024-01-26 18:45:59 +0800

    install-mongoose-and-create-data-with-Shchma-and-model

--- app.js
+++ app.js
@@ -1,17 +1,43 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+//db url
+const mongoUrl =
+  "mongodb+srv://davidsang:david1234@cluster0.icwxdkm.mongodb.net/?retryWrites=true&w=majority";
+mongoose
+  .connect(mongoUrl)
+  .then(() => {
+    console.log("connected to db");
+    app.listen(port, () => {
+      console.log("server is running now");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 const port = 3000;
 app.set("views", "./views");
 app.set("view engine", "ejs");
 const morgan = require("morgan");
 app.use(morgan("dev"));
 app.use(express.static("public"));
+const Blog = require("./models/Blog");
+
+app.get("/add-blog", async (req, res) => {
+  const blog = new Blog({
+    title: "blog title 2",
+    intro: "blog intro 2",
+    body: "blog body 2",
+  });
+  await blog.save();
+  res.send("blog saved");
+});
 
 app.get("/", (req, res) => {
   const blogs = [
     { title: "blog title 1", intro: "this is blog 1 intro" },
     { title: "blog title 2", intro: "this is blog 2 intro" },
-    { title: "blog title 3", intro: "this is blog 3 intro" },
   ];
 
   res.render("home", {
@@ -32,5 +58,3 @@ app.use((req, res) => {
   res.status(404).render("404", { title: "404 not found" });
   r;
 });
-
-app.listen(port);
